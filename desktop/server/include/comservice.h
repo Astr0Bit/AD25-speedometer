2221d2369ec1767a5322876335e13a76859e3299
Emil Rydberg <the.gillarski@gmail.com>
2026-06-10 10:32:23 +0200

    Added method-declarations, buffer, mutex and atomic bool for COMService abstract class for server

--- desktop/server/include/comservice.h
+++ desktop/server/include/comservice.h
@@ -6,8 +6,50 @@
 #include <cstdint>
 #include "setting.h"
 
+// * Abstract class for communication using multithreading
 class COMService
 {
+protected:
+    // * Buffer mutex, and atomic variable for class
+    uint8_t m_buf[SBUFLEN]{0};
+    mutable std::mutex m_mtx;
+    std::atomic_bool m_is_connected{false};
+
+public:
+    // * Methods for inserting:
+    /**
+     * @brief Insert speed (Kph) into buffer
+     *
+     * @param speed
+     */
+    void insertSpeed(uint8_t speed_kph);
+
+    /**
+     * @brief Insert temperature (°C) into buffer
+     *
+     * @param temp
+     */
+    void insertTemp(int8_t temp_c);
+
+    /**
+     * @brief Insert battery level (%) into buffer
+     *
+     * @param bat_prc
+     */
+    void insertBattery(uint8_t bat_prc);
+
+    /**
+     * @brief Insert light signals into buffer
+     *
+     * @param is_on
+     */
+    void insertLightSignals(bool left, bool right);
+
+    // * Send buffer
+    virtual void sendBuffer(void) = 0;
+
+    // * Get communication status
+    virtual bool getStatus() const = 0;
 };
 
 #endif
