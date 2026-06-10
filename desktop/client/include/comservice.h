1d551de2ba0ee2b059c4cc43c928f7e2a709406c
Erik Johansson <erik.alexius.johansson@gmail.com>
2026-06-10 19:27:23 +0200

    almost finished

--- desktop/client/include/comservice.h
+++ desktop/client/include/comservice.h
@@ -8,6 +8,42 @@
 
 class COMService
 {
+protected:
+    COMService();
+    virtual ~COMService() = default;
+public:
+
+    void extractSpeed(uint8_t& out);
+    void extractTemp(int8_t& out);
+    void extractBattery(uint8_t& out);
+    void extractLightSignals(bool& out_left, bool& out_right);
+
+    virtual void receiveBuffer(void) = 0;
+    virtual bool getStatus() const = 0;
+
+protected:
+    std::atomic_bool m_is_connected{false};
+
+    class BufferGuard {
+    private:
+        friend class COMService;
+        BufferGuard(uint8_t(&buf)[SBUFLEN], std::mutex& mtx);
+        BufferGuard(BufferGuard&&) = default;
+        BufferGuard& operator=(BufferGuard&&) = default;
+
+        std::unique_lock<std::mutex> m_lock;
+    public:
+        uint8_t(&buffer)[SBUFLEN];
+    };
+
+    BufferGuard lockBuffer();
+
+private:
+    std::mutex m_mtx;
+    uint8_t m_buffer[SBUFLEN]{};
+    const Setting::Signal& m_signal;
+
+    uint8_t extract_nbits(uint32_t n, uint32_t off);
 };
 
 #endif
--- desktop/client/src/comservice.cpp
+++ desktop/client/src/comservice.cpp
@@ -1,2 +1,55 @@
 #include <climits>
+#include <cstring>
 #include "comservice.h"
+
+COMService::COMService()
+    : m_signal{Setting::Signal::handle()}
+{
+}
+
+COMService::BufferGuard::BufferGuard(uint8_t(&buf)[SBUFLEN], std::mutex& mtx)
+    : buffer{buf}, m_lock{mtx}
+{
+}
+
+COMService::BufferGuard COMService::lockBuffer()
+{
+    return COMService::BufferGuard{m_buffer, m_mtx};
+}
+
+inline uint8_t COMService::extract_nbits(uint32_t bit_len, uint32_t bit_start)
+{
+    auto locked = lockBuffer();
+    const uint32_t byte_idx = bit_start >> 3;
+    const uint32_t bit_off = bit_start & 7U;
+
+    const uint16_t lo = (uint16_t)locked.buffer[byte_idx];
+    const uint16_t hi = (bit_off + bit_len > 8U ? (uint16_t)locked.buffer[byte_idx + 1] << 8 : 0U);
+
+    return (uint8_t)(((lo | hi) >> bit_off) & ((1U << bit_len) - 1U));
+}
+
+void COMService::extractSpeed(uint8_t& out)
+{
+    const auto& si = m_signal["speed"];
+    out = extract_nbits(si.length, si.start);
+}
+void COMService::extractTemp(int8_t& out)
+{
+    const auto& si = m_signal["temperature"];
+    const uint8_t raw = extract_nbits(si.length, si.start);
+    const uint8_t sign_bit = static_cast<uint8_t>(1U << (si.length - 1));
+    out = static_cast<int8_t>((raw ^ sign_bit) - sign_bit);
+}
+void COMService::extractBattery(uint8_t& out)
+{
+    const auto& si = m_signal["battery_level"];
+    out = extract_nbits(si.length, si.start);
+}
+void COMService::extractLightSignals(bool& out_left, bool& out_right)
+{
+    const auto& si_l = m_signal["left_light"];
+    const auto& si_r = m_signal["right_light"];
+    out_left = !!extract_nbits(si_l.length, si_l.start);
+    out_right = !!extract_nbits(si_r.length, si_r.start);
+}
