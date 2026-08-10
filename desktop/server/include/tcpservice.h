f0f9ec02bbf359dd1ddfd1eaaf040f48f1313015
Emil Rydberg <the.gillarski@gmail.com>
2026-08-10 14:26:29 +0200

    Began working on TCPService class for server.

--- desktop/server/include/tcpservice.h
+++ desktop/server/include/tcpservice.h
@@ -6,10 +6,26 @@
 #include <sys/socket.h>
 #include "comservice.h"
 
+// TODO -> These should be put inside shared/setting.h instead
+constexpr int N_CONNS{1};
+constexpr int PORT{1337};
+constexpr int SEND_INTERVAL_MS{20};
+
 class TCPService : public COMService
 {
-    // * Just for testing
-    void run(void) override {};
+private:
+    /**
+     * @brief Main thread for TCPService, reads the buffer, and periodically sends it over TCP / IP
+     *
+     */
+    void serverWorker(void);
+
+public:
+    /**
+     * @brief Creates the TCP/IP socket, spawns the main worker thread, and handles errors
+     *
+     */
+    void run(void) override;
 };
 
 #endif
--- desktop/server/src/tcpservice.cpp
+++ desktop/server/src/tcpservice.cpp
@@ -3,6 +3,75 @@
 
 #include <QDebug>
 #include <netdb.h>
+#include <iostream>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+
+class TCPService : public COMService
+{
+    private:
+    void serverWorker(void) {
+        
+    }
+
+    public:
+    void run(void) override
+    {
+        // Attempt to create the TCP / IP socket
+        int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+        if (sockfd == -1)
+        {
+            std::cout << "Failed to create TCP/IP socket, exiting...\n";
+            std::exit(EXIT_FAILURE);
+        }
+
+        // Create an internet socket address
+        sockaddr_in servaddr{0};
+
+        // Assign IP and PORT
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(PORT);
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        // Bind the socket address to the socket
+        if (0 == bind(sockfd, (sockaddr *)&servaddr, sizeof(servaddr)))
+        {
+            // TODO -> Likely to be put in a thread instead, ie. constantly listen
+            // TODO -> What if the connection silently drops?
+            if (0 == listen(sockfd, N_CONNS))
+            {
+                // Grab IP and length of incomming packet
+                sockaddr_in cli{0};
+                socklen_t len = sizeof(cli);
+
+                int connfd = accept(sockfd, (sockaddr *)&cli, &len);
+
+                // Established connection
+                if (connfd >= 0)
+                {
+                    std::cout << "Server accepted the client...\n";
+                    m_is_connected = true;
+
+                    // TODO -> Periodically send the COMService buffer
+                }
+                else
+                {
+                    std::cout << "Failed to accept the connection...\n";
+                }
+            }
+            else
+            {
+                std::cout << "Failed to listen to the port...\n";
+            }
+        }
+        else
+        {
+            std::cout << "Failed to bind servaddr to the socket...\n";
+        }
+
+        // Close the socket
+        shutdown(sockfd, SHUT_RDWR);
+        close(sockfd);
+    }
+}
