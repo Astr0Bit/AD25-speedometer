46c585e51d27bb1fed0b2e1efca98a7841988c7a
Emil Rydberg <the.gillarski@gmail.com>
2026-08-25 10:38:10 +0200

    Update esp32/server/src/main.c: - Made `service_gatt_handler` just return 0 since terminal output is not required - Removed all ESP_LOGI usages - Removed print_conn_desc - Replaced some ESP_LOGI with ESP_LOGE

--- esp32/server/src/main.c
+++ esp32/server/src/main.c
@@ -37,7 +37,7 @@ static const char *TAG = "SERVER";
 
 // Function declarations
 static int gap_event(struct ble_gap_event *event, void *arg);
-static int service_gatt_handler(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg);
+static int service_gatt_handler(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg) { return 0; }
 
 static uint8_t addr_type;
 static uint16_t chrval_handle;
@@ -79,24 +79,6 @@ static const struct ble_gatt_svc_def ble_svc_gatt_defs[] = {
         /* No more services. */
     },
 };
-
-// Logs information about a connection to the console.
-static void print_conn_desc(const struct ble_gap_conn_desc *desc)
-{
-    char addr[18];
-
-    sprintf(addr, "%02x:%02x:%02x:%02x:%02x:%02x", desc->our_id_addr.val[5], desc->our_id_addr.val[4],
-            desc->our_id_addr.val[3], desc->our_id_addr.val[2], desc->our_id_addr.val[1], desc->our_id_addr.val[0]);
-    ESP_LOGI(TAG, " our_id_addr_type=%d our_id_addr=%s", desc->our_id_addr.type, addr);
-
-    sprintf(addr, "%02x:%02x:%02x:%02x:%02x:%02x", desc->peer_id_addr.val[5], desc->peer_id_addr.val[4],
-            desc->peer_id_addr.val[3], desc->peer_id_addr.val[2], desc->peer_id_addr.val[1], desc->peer_id_addr.val[0]);
-    ESP_LOGI(TAG, " peer_id_addr_type=%d peer_id_addr=%s", desc->peer_id_addr.type, addr);
-
-    ESP_LOGI(TAG, " conn_itvl=%d conn_latency=%d supervision_timeout=%d encrypted=%d authenticated=%d bonded=%d\n",
-             desc->conn_itvl, desc->conn_latency, desc->supervision_timeout, desc->sec_state.encrypted,
-             desc->sec_state.authenticated, desc->sec_state.bonded);
-}
 
 static void advertise(void)
 {
@@ -152,11 +134,7 @@ static void advertise(void)
 
     /* Start advertising */
     status = ble_gap_adv_start(addr_type, NULL, BLE_HS_FOREVER, &adv_params, gap_event, NULL);
-    if (status == 0)
-    {
-        ESP_LOGI(TAG, "Advertising started!");
-    }
-    else
+    if (status != 0)
     {
         ESP_LOGE(TAG, "Failed to start advertising, error code: %d", status);
     }
@@ -170,14 +148,12 @@ static int gap_event(struct ble_gap_event *event, void *)
     switch (event->type)
     {
     case BLE_GAP_EVENT_CONNECT: /* A new connection was established or a connection attempt failed. */
-        ESP_LOGI(TAG, "connection %s; status=%d ", event->connect.status == 0 ? "established" : "failed", event->connect.status);
         if (event->connect.status == 0)
         {
             // Save current connection handle
             active_conn_handle = event->connect.conn_handle;
 
             assert(0 == ble_gap_conn_find(event->connect.conn_handle, &desc));
-            print_conn_desc(&desc);
         }
         else
         {
@@ -187,47 +163,32 @@ static int gap_event(struct ble_gap_event *event, void *)
         break;
 
     case BLE_GAP_EVENT_DISCONNECT:
-        ESP_LOGI(TAG, "disconnect; reason=%d ", event->disconnect.reason);
-
         // Clear handle on disconnect
         active_conn_handle = NO_CONN_HANDLE;
-
-        print_conn_desc(&event->disconnect.conn);
         advertise(); /* Connection terminated; resume advertising. */
         break;
 
     case BLE_GAP_EVENT_CONN_UPDATE: /* The central has updated the connection parameters. */
-        ESP_LOGI(TAG, "connection updated; status=%d ", event->conn_update.status);
         assert(0 == ble_gap_conn_find(event->conn_update.conn_handle, &desc));
-        print_conn_desc(&desc);
         break;
 
     case BLE_GAP_EVENT_ADV_COMPLETE:
-        ESP_LOGI(TAG, "advertise complete; reason=%d", event->adv_complete.reason);
         advertise();
         break;
 
     case BLE_GAP_EVENT_SUBSCRIBE:
-        ESP_LOGI(TAG, "Subscribe event; conn_handle=%d attr_handle=%d  reason=%d prevn=%d curn=%d previ=%d curi=%d\n",
-                 event->subscribe.conn_handle, event->subscribe.attr_handle, event->subscribe.reason, event->subscribe.prev_notify,
-                 event->subscribe.cur_notify, event->subscribe.prev_indicate, event->subscribe.cur_indicate);
         break;
 
     case BLE_GAP_EVENT_REPEAT_PAIRING:                                            /* Repeat pairing event */
         assert(0 == ble_gap_conn_find(event->repeat_pairing.conn_handle, &desc)); /* Get connection descriptor */
         assert(0 == ble_store_util_delete_peer(&desc.peer_id_addr));              /* Delete the old bond */
 
-        ESP_LOGI(TAG, "Repairing...");
         status = BLE_GAP_REPEAT_PAIRING_RETRY; // Return BLE_GAP_REPEAT_PAIRING_RETRY to indicate that
                                                // the host should continue with pairing operation
         break;
 
     case BLE_GAP_EVENT_ENC_CHANGE: /* Encryption change event: Encryption has been enabled or disabled for this connection. */
-        if (event->enc_change.status == 0)
-        {
-            ESP_LOGI(TAG, "connection encrypted!");
-        }
-        else
+        if (event->enc_change.status != 0)
         {
             ESP_LOGE(TAG, "connection encryption failed, status: %d", event->enc_change.status);
         }
@@ -235,8 +196,6 @@ static int gap_event(struct ble_gap_event *event, void *)
 
     case BLE_GAP_EVENT_PASSKEY_ACTION:
     {
-        ESP_LOGI(TAG, "Passkey Action. Action Type: %d", event->passkey.params.action);
-
         struct ble_sm_io pkey = {0};
         pkey.action = event->passkey.params.action;
 
@@ -286,46 +245,17 @@ static void on_sync(void)
     advertise();
 }
 
-/* Callback function for custom service */
-static int service_gatt_handler(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *)
-{
-    switch (ctxt->op)
-    {
-    case BLE_GATT_ACCESS_OP_READ_CHR:
-        ESP_LOGI(TAG, "Callback for read");
-        break;
-
-    case BLE_GATT_ACCESS_OP_WRITE_CHR:
-    {
-        ESP_LOGI(TAG, "Data received in write event, conn_handle = %x, attr_handle = %x", conn_handle, attr_handle);
-    }
-    break;
-
-    default:
-        ESP_LOGI(TAG, "\nDefault Callback");
-        break;
-    }
-
-    return 0;
-}
-
 static void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *)
 {
-    char buf[BLE_UUID_STR_LEN] = {0};
-
     switch (ctxt->op)
     {
     case BLE_GATT_REGISTER_OP_SVC:
-        ESP_LOGI(TAG, "registered service %s with handle=%d\n", ble_uuid_to_str(ctxt->svc.svc_def->uuid, buf), ctxt->svc.handle);
         break;
 
     case BLE_GATT_REGISTER_OP_CHR:
-        ESP_LOGI(TAG, "registering characteristic %s with def_handle=%d val_handle=%d\n",
-                 ble_uuid_to_str(ctxt->chr.chr_def->uuid, buf), ctxt->chr.def_handle, ctxt->chr.val_handle);
         break;
 
     case BLE_GATT_REGISTER_OP_DSC:
-        ESP_LOGI(TAG, "registering descriptor %s with handle=%d\n", ble_uuid_to_str(ctxt->dsc.dsc_def->uuid, buf), ctxt->dsc.handle);
         break;
 
     default:
@@ -401,7 +331,6 @@ static ble_err_t ble_setup()
 // Task to make BLE non-blocking
 static void ble_host_task(void *param)
 {
-    ESP_LOGI(TAG, "BLE Host Task Started");
     nimble_port_run();
     nimble_port_freertos_deinit();
 }
@@ -475,8 +404,6 @@ void app_main(void)
     ESP_ERROR_CHECK(uart_param_config(UART, &config));
     ESP_ERROR_CHECK(uart_set_pin(UART, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 
-    ESP_LOGI(TAG, "UART initialized");
-
     // * To store received event
     uart_event_t event;
     uint8_t buffer[SBUFLEN];
@@ -532,7 +459,6 @@ void app_main(void)
                         {
                             led_state = !led_state;
                             ESP_ERROR_CHECK(gpio_set_level(LED_PIN, led_state));
-                            ESP_LOGI(TAG, "Notification sent successfully");
                         }
                         else
                         {
@@ -547,33 +473,32 @@ void app_main(void)
                 }
 
                 case UART_FIFO_OVF: /* Event of HW FIFO overflow detected */
-                    ESP_LOGI(TAG, "Hardware FIFO overflow");
+                    ESP_LOGE(TAG, "Hardware FIFO overflow");
                     // If fifo overflow happened, you should consider adding flow control for your application.
                     uart_flush_input(UART);
                     xQueueReset(queue);
                     break;
 
                 case UART_BUFFER_FULL: /* Event of UART ring buffer full */
-                    ESP_LOGI(TAG, "Ring buffer full");
+                    ESP_LOGE(TAG, "Ring buffer full");
                     // If buffer full happened, you should consider increasing your buffer size
                     uart_flush_input(UART);
                     xQueueReset(queue);
                     break;
 
                 case UART_BREAK: /* Event of UART RX break detected */
-                    ESP_LOGI(TAG, "UART rx break");
+                    ESP_LOGE(TAG, "UART rx break");
                     break;
 
                 case UART_PARITY_ERR: /* Event of UART parity check error */
-                    ESP_LOGI(TAG, "UART parity error");
+                    ESP_LOGE(TAG, "UART parity error");
                     break;
 
                 case UART_FRAME_ERR: /* Event of UART frame error */
-                    ESP_LOGI(TAG, "UART frame error");
+                    ESP_LOGE(TAG, "UART frame error");
                     break;
 
                 default: /* Others */
-                    ESP_LOGI(TAG, "UART event type: %d", event.type);
                     break;
                 }
             }
